79e3a7588952032807a81150d40f92c40c50e724
AhKui <ahkui@outlook.com>
2017-01-17 21:15:11 +0800

    encrtpy password

--- js/custom.js
+++ js/custom.js
@@ -52,7 +52,8 @@ function _search() {
             $('body').text('');
             $('body').append(data.html);
             _initLoginForm();
-            $.cookie('key','', { expires: 0, path: '/' })
+            $.removeCookie('key', { path: '/' })
+            $.removeCookie('session', { path: '/' })
         })
         .fail(function(data) {
             console.log("error", data);
@@ -77,7 +78,17 @@ function _initLoginForm() {
     });
 }
 
+function navMaxHeight() {
+    $('.navbar-toggleable-xs').css('max-height', _height() - 54);
+}
+$(window).resize(function(event) {
+    if ($.cookie('key')) {
+        navMaxHeight();
+    }
+});
+
 function _initMain() {
+    navMaxHeight();
     // $("ul.nav li.dropdown").hover(function() {
     //     $(this).find(".dropdown-menu").stop(!0, !0).delay(50).fadeIn(100), $(this).find("a").attr("aria-expanded", "true"), $(this).addClass("open")
     // }, function() {
@@ -97,7 +108,12 @@ function _login(form) {
                     $('body').append(data.html);
                     _initMain();
                     if (data.key) {
-                        $.cookie('key',data.key, { expires: 365, path: '/' })
+                        $.cookie('key', data.key, { expires: 365, path: '/' })
+                    } else {
+                        $.cookie('session', data.session, { path: '/' })
+                        window.onbeforeunload = function(evt) {
+                            $.removeCookie('session', { path: '/' })
+                        }
                     }
                 } else {
                     $(form).find('[type=submit]').prop('disabled', false).removeClass('error success clicked').html(msg['btn-default']);
@@ -113,4 +129,16 @@ function _login(form) {
         form_loading($(form));
     }
     return false;
+}
+
+function scrolltop() {
+    $(document).scrollTop(0)
+}
+
+function _width() {
+    return window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth
+}
+
+function _height() {
+    return window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName("body")[0].clientHeight
 }
--- login.php
+++ login.php
@@ -1,10 +1,92 @@
 <?php
-$main = '<nav class="navbar navbar-dark bg-inverse navbar-fixed-top"><button class="navbar-toggler hidden-sm-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="/**/"></button><a class="navbar-brand" href onclick="mainpage(); event.preventDefault();">首頁</a><div class="collapse navbar-toggleable-xs" id="navbarResponsive"><ul class="nav navbar-nav"><li class="nav-item" onclick><a class="nav-link">地圖<span class="sr-only">(current)</span></a></li><li style="top:54px;" class="nav-item" onclick><a class="nav-link">食物<span class="sr-only">(current)</span></a></li><li class="nav-item" onclick><a class="nav-link">旅遊</a></li><li class="nav-item"><a class="nav-link" href="#">地區</a></li><li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a><div class="dropdown-menu"><a class="dropdown-item" href="#" onclick="alert();">Action</a><div class="dropdown-divider"></div><h6 class="dropdown-header">Dropdown header</h6><a class="dropdown-item" href="#">Another action</a></div></li></ul><form class="form-inline col-xs-12 col-sm-5 col-md-4 col-lg-3 float-sm-right" style="padding:0" onsubmit="_search();return false;"><button class="btn btn-outline-success" type="submit" style="width:25%">登出</button></form></div></nav><div class="container-fluid"></div>';
-$login = '<div class="text-center" id="login" class="col-xs-4 offset-xs-4"><div class="logo">login</div><div class="login-form-1"><form id="login-form" class="text-left"><div class="login-form-main-message"></div><div class="main-login-form"><div class="login-group"><div class="form-group"><label for="username" class="sr-only">Username</label><input type="text" class="form-control" id="username" name="username" placeholder="username"></div><div class="form-group"><label for="password" class="sr-only">Password</label><input type="password" class="form-control" id="password" name="password" placeholder="password"></div><div class="form-group login-group-checkbox"><input type="checkbox" id="remember" name="remember"><label for="remember">remember</label></div></div><button type="submit" class="login-button" style="cursor: pointer;"><i class="fa fa-chevron-right"></i></button></div><div class="etc-login-form"><p><a href="#">forgot your password?</a></p></div></form></div></div>';
+$main = '<nav class="navbar navbar-dark bg-inverse navbar-fixed-top">
+    <button class="navbar-toggler hidden-sm-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="/**/"></button>
+    <a class="navbar-brand" href onclick="mainpage(); event.preventDefault();"">首頁</a>
+    <div class="collapse navbar-toggleable-xs" id="navbarResponsive">
+        <ul class="nav navbar-nav">
+            <li class="nav-item" onclick>
+                <a class="nav-link">地圖
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li style="top:54px;" class="nav-item" onclick>
+                <a class="nav-link">食物
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item" onclick>
+                <a class="nav-link">旅遊</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">地區</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#" onclick="alert();">Action</a>
+                    <div class="dropdown-divider"></div>
+                    <h6 class="dropdown-header">Dropdown header</h6>
+                    <a class="dropdown-item" href="#">Another action</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline col-xs-12 col-sm-5 col-md-4 col-lg-3 float-sm-right" style="padding:0" onsubmit="_search();return false;">
+            <button class="btn btn-outline-success" type="submit" style="width:25%">登出</button>
+        </form>
+    </div>
+</nav>
+<div class="container-fluid"></div>
+';
+$login = '<div class="text-center" id="login" class="col-xs-4 offset-xs-4">
+    <div class="logo">login</div>
+    <div class="login-form-1">
+        <form id="login-form" class="text-left">
+            <div class="login-form-main-message"></div>
+            <div class="main-login-form">
+                <div class="login-group">
+                    <div class="form-group">
+                        <label for="username" class="sr-only">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="sr-only">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="password">
+                    </div>
+                    <div class="form-group login-group-checkbox">
+                        <input type="checkbox" id="remember" name="remember">
+                        <label for="remember">remember</label>
+                    </div>
+                </div>
+                <button type="submit" class="login-button" style="cursor: pointer;"><i class="fa fa-chevron-right"></i></button>
+            </div>
+            <div class="etc-login-form">
+                <p>
+                    <a href="#">forgot your password?</a>
+                </p>
+            </div>
+        </form>
+    </div>
+</div>
+';
 // status value meaning
 // 0 -> login fail
 // 1 -> login success
 // 9 -> logout
+
+if($_GET){
+    echo crypt('ahkui', '$6$ahkui$ahkui$').'<br>';
+    require_once 'db_connect.php';
+    $query = $db->prepare("SELECT * FROM `user` WHERE `username` = ?");
+    $asd = 'ahkui';
+    $query->bind_param("s", $asd);
+    $query->execute();
+    $query->store_result();
+    $query->bind_result($id,$username, $password,$level,$loginpcname,$loginip,$loginbrowser,$logintime,$loginkey);
+    $query->fetch();
+    echo($id.'<br>'.$username.'<br>'. $password.'<br>'.$level.'<br>'.$loginpcname.'<br>'.$loginip.'<br>'.$loginbrowser.'<br>'.$logintime.'<br>'.$loginkey);
+    echo "<br>".$_COOKIE['key'];
+    exit();
+}
 if ( isset($_POST) && !empty($_POST) ) {
     $data = array();
     if ( isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password']) ) {
@@ -13,15 +95,26 @@ if ( isset($_POST) && !empty($_POST) ) {
         $query->bind_param("ss", $username,$password);
         $username = $_POST['username'];
         $password = $_POST['password'];
+        $password = crypt($password, '$6$'.$username.'$'.$password.'$');
         $query->execute();
         $query->store_result();
         if($query->num_rows()===1){
-            $query->bind_result($id,$username, $password,$level,$loginip,$loginbrowser,$logintime,$loginkey);
+            $query->bind_result($id,$username, $password,$level,$loginpcname,$loginip,$loginbrowser,$logintime,$loginkey);
             $query->fetch();
+            $clientname = gethostname();
+            $clientip = ipCheck();
+            $clientrequesttime = $_SERVER['REQUEST_TIME'];
+            $clientbrowser = $_SERVER['HTTP_USER_AGENT'];
+            $md5 = md5($id.$username.$password.$level.$clientname.$clientip.$clientbrowser.$clientrequesttime.$loginkey);
+            $key = crypt($password, '$6$'.$md5.'$'.$loginkey.'$');
+            $loginQuery = $db->prepare("update `user` set `loginip`=?,`loginbrowser`=?,`logintime`=?,`loginkey`=?,`loginpcname`=? where `id`=$id");
+            $loginQuery->bind_param("ssiss",$clientip,$clientbrowser,$clientrequesttime,$key,$clientname);
+            $loginQuery->execute();
             $data = array('html'=>$main,'status'=>1);
-            $key = md5($id.$username.$password.$level.$loginip.$loginbrowser.$logintime.$loginkey);
             if ( isset($_POST['remember']) && !empty($_POST['remember']) ) {
                 $data['key'] = $key;
+            }else{
+                $data['session'] = $key;
             }
         }else{
             $data = array('status'=>0);
@@ -52,7 +145,11 @@ if ( isset($_POST) && !empty($_POST) ) {
         <script src="js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
         <script src="js/custom.js" type="text/javascript" charset="utf-8" async defer></script>
     </head>
-    <body><?php if( isset($_COOKIE['key']) && !empty($_COOKIE['key']) ) { main(); } else { login(); } ?></body>
+
+    <body>
+        <?php if( isset($_COOKIE['key']) && !empty($_COOKIE['key']) ) { main(); } else { login(); } ?>
+    </body>
+
     </html>
     <?php
 }
@@ -62,4 +159,25 @@ function login(){
 function main(){ 
     echo $GLOBALS['main'];
 }
+function ipCheck() {
+        if (getenv('HTTP_CLIENT_IP')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        }
+        elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        }
+        elseif (getenv('HTTP_X_FORWARDED')) {
+            $ip = getenv('HTTP_X_FORWARDED');
+        }
+        elseif (getenv('HTTP_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_FORWARDED_FOR');
+        }
+        elseif (getenv('HTTP_FORWARDED')) {
+            $ip = getenv('HTTP_FORWARDED');
+        }
+        else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 ?>
